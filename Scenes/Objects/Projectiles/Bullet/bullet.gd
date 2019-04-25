f35fa76b62f0c55f93438dd055d67c5cdedbce03
evsey9 <evseyru1@mail.ru>
2019-04-26 01:55:45 +0500

    more porting from platformer ( will replcae later )

---
+++ Scenes/Objects/Projectiles/Bullet/bullet.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var col : KinematicCollision2D
+var move : Vector2 = Vector2(0,0) 
+var speed = 100
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	col = move_and_collide(move * speed * delta)
+	if col:
+		hit(col.collider)
+
+func hit(body):
+	if body.has_method("hit_by_bullet"):
+		body.call("hit_by_bullet")
+	queue_free()
+func _on_Timer_timeout():
+	queue_free()
